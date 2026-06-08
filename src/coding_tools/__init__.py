fea9a2313f7b7e580095efd38369bf113598c966
Sebastien Lenard <sebastien.lenard@gmail.com>
2026-06-08 09:29:39 -0600

    path_stamper.py -> workspace_stamper.py

--- src/coding_tools/__init__.py
+++ src/coding_tools/__init__.py
@@ -1,5 +1,5 @@
 # src/coding_tools/__init__.py
 from .backup_untracked import UntrackedBackupManager
-from .path_stamper import PathStamper
+from .workspace_stamper import PathStamper
 
 __all__ = ["PathStamper", "UntrackedBackupManager"]
