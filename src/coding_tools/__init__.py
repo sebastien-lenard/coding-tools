f9953e720f9de16ca3db48263a04317afe76f787
Sebastien Lenard <sebastien.lenard@gmail.com>
2026-06-08 17:33:10 -0600

    README/pyproject: add description of workspace_stamp license functionalities + extend ruff lint rules

--- src/coding_tools/__init__.py
+++ src/coding_tools/__init__.py
@@ -1,4 +1,6 @@
 # src/coding_tools/__init__.py
+# SPDX-FileCopyrightText: 2026 Sebastien Lenard <sebastien.lenard@gmail.com> and Contributors
+# SPDX-License-Identifier: Apache-2.0
 from .backup_untracked import UntrackedBackupManager
 from .workspace_stamper import WorkspaceStamper
 
--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,1 +1,3 @@
 # tests/unit/__init__.py
+# SPDX-FileCopyrightText: 2026 Sebastien Lenard <sebastien.lenard@gmail.com> and Contributors
+# SPDX-License-Identifier: Apache-2.0
